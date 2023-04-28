5bf1105f396710ec2931b808da6246e3021d65d3
Masum Billah <masumbillahyusuf1621@gmail.com>
2023-04-28 19:21:57 +0600

    get data  from express server side and showing data in UI

--- src/pages/Shared/LeftNav/LeftNav.jsx
+++ src/pages/Shared/LeftNav/LeftNav.jsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const LeftNav = () => {
-    return (
-        <div>
-            <h2>Left nav</h2>
-        </div>
-    );
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/categories")
+      .then((res) => res.json())
+      .then((data) => setCategories(data))
+      .catch((err) => console.error(err));
+  }, []);
+
+  return (
+    <div>
+      <h4 className="mb-4">All Category</h4>
+      <p className="bg-secondary bg-opacity-10 fw-semibold fs-5 w-75 px-5 py-3 rounded">National News</p>
+      {categories.map((category) => (
+        <p key={category.id} className="px-5">
+          <Link
+            to={`/category/${category.id}`}
+            className="text-secondary text-center fw-semibold fs-5 text-decoration-none"
+          >
+            {category.name}
+          </Link>
+        </p>
+      ))}
+    </div>
+  );
 };
 
 export default LeftNav;
