880f1025aaa8f65d5bd7dca681980d519485ae38
Masum Billah <masumbillahyusuf1621@gmail.com>
2023-04-28 17:15:31 +0600

    navbar add

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,18 +1,52 @@
 import React from "react";
-import logo from "../../../assets/logo.png";
 import moment from "moment";
+import Marquee from "react-fast-marquee";
+import logo from "../../../assets/logo.png";
+import Button from "react-bootstrap/Button";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { UserCircleIcon } from "@heroicons/react/24/solid";
+import "./Header.css";
 
 const Header = () => {
   return (
-    <div>
+    <Container>
       <div className="text-center mt-4">
-        <img src={logo} alt="website logo" />
+        <img src={logo} alt="website logo" className=" w-75" />
         <p className="text-secondary mt-2">
           <small>Journalism Without Fear or Favour</small> <br />
-          <small className="fw-bold">{moment().format("dddd, MMMM D, YYYY")}</small>
+          <small className="fw-bold">
+            {moment().format("dddd, MMMM D, YYYY")}
+          </small>
         </p>
       </div>
-    </div>
+      <div className="d-flex bg-secondary bg-opacity-10 p-2 rounded my-4">
+        <Button variant="danger">Latest</Button>
+        <Marquee className="text-danger" speed={100} pauseOnHover={true}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. At quasi
+          exercitationem maxime hic eum provident!
+        </Marquee>
+      </div>
+      <Navbar expand="lg" className="mb-5">
+        <Container>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto fs-5">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#about">About</Nav.Link>
+              <Nav.Link href="#career">Career</Nav.Link>
+            </Nav>
+            <Nav className="ms-auto">
+              <Nav.Link href="#profile">
+                <UserCircleIcon className="user-icon" />
+              </Nav.Link>
+              <Nav.Link href="#login">
+                <Button className="px-4" variant="secondary">Login</Button>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </Container>
   );
 };
 
