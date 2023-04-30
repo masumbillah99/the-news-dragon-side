1f0eb48d8c59b03151054fd3b1165a1ec1cd3656
Masum Billah <masumbillahyusuf1621@gmail.com>
2023-04-30 22:01:23 +0600

    firebase config

--- src/pages/Login/Login/Login.jsx
+++ src/pages/Login/Login/Login.jsx
@@ -8,7 +8,7 @@ const Login = () => {
   const { signInUser } = useContext(AuthContext);
   const navigate = useNavigate();
   const location = useLocation();
-  console.log("login page", location);
+  // console.log("login page", location);
   const from = location.state?.from?.pathname || "/category/0";
 
   const [error, setError] = useState("");
